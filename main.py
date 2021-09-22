48d25917ecc4d667db7f5b6160ab9ab2cf5636b2
Serhii Iljin <89367614+serhii-iljin@users.noreply.github.com>
2021-09-22 20:31:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     bot.send_message(message.chat.id, message.text[0])
+    bot.send_message(message.chat.type)
     if (message.text[0] != '!') and ((message.chat.type == 'supergroup') or (message.chat.type == 'group')):
+        bot.send_message("nope")
         return
     if (message.text[0] == '!') and ((message.chat.type == 'supergroup') or (message.chat.type == 'group')):
+        bot.send_message("yeah!")
         q = message.text[1:]
     else:
         q = message.text
