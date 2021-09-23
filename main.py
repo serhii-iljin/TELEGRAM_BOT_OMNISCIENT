9d6035d6990d1189eddf3312654192fee61a9dc5
Serhii Iljin <89367614+serhii-iljin@users.noreply.github.com>
2021-09-23 18:51:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,49 +2,54 @@ import telebot
 from telebot import types
 import wolframalpha
 
-bot = telebot.TeleBot('1930969642:AAEH0CC-EYezvPVOvuM45OEg4_L8Wjproxo')
+bot = telebot.TeleBot('936898084:AAFgPrxOR_jEVd3fEV4qQ96s8oJ70-kbdhE')
 
-@bot.message_handler(commands=['help'])
+
+@bot.message_handler(commands=['help', 'helpOmn'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Just type your question in, come on!\nIf you use this bot in group chats just type \"!\" before your question.\nAll commands: /help, /start, /contacts, /review, /examples.')
+    bot.send_message(message.chat.id, 'Just type your question in, come on!\nIf you use this bot in group chats just type \"!\" before your question.\nAll commands: /help, /start, /contacts, /review, /examples.\nUse \"Omn\" in the end to interact with the bot if you have other bots in a group chat.')
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=['start', 'startOmn'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'This bot can answer every your question!\nAll commands: /help, /start, /contacts, /review, /examples.')
+    bot.send_message(message.chat.id, 'This bot can answer every your question!\nAll commands: /help, /start, /contacts, /review, /examples.\nUse \"Omn\" in the end to interact with the bot if you have other bots in a group chat.')
 
-@bot.message_handler(commands=['contacts'])
+
+@bot.message_handler(commands=['contacts', 'contactsOmn'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Developer:\n-Telegram: @serhii_iljin;\n-GitHub: https://github.com/serhii-iljin,\n-LinkedIn: https://www.linkedin.com/in/%D1%81%D0%B5%D1%80%D0%B3%D1%96%D0%B9-%D1%96%D0%BB%D1%8C%D1%97%D0%BD-aaaa3b219/\nLogo designed by @Andrii_e_o.')
 
-@bot.message_handler(commands=['examples'])
+
+@bot.message_handler(commands=['examples', 'examplesOmn'])
 def start_message(message):
     bot.send_message(message.chat.id, 'General questions:\n-Temperature in Kherson?\n-Weather in kherson?\n-What is your name?\n\nMaths questions:\n-Solve x^5-x^3+3x^2-3=0\n-Integral x^2+ln(x)-1/x-32\n-Derivative x^3+sqrt(x)')
 
-@bot.message_handler(commands=['review'])
+
+@bot.message_handler(commands=['review', 'reviewOmn'])
 def start_message(message):
     if (message.chat.type == 'supergroup') or (message.chat.type == 'group'):
-        bot.send_message(message.chat.id,"Write your review directly in the bot.")
+        bot.send_message(message.chat.id, "Write your review directly in the bot.")
         return
     markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
     dismiss = types.KeyboardButton('Cancel')
     sendMessage = types.KeyboardButton('Send message')
     markup.add(dismiss)
     markup.add(sendMessage)
-    bot.send_message(message.chat.id, "Please, write your review with \"#\" at the beggining in the next message ans press \"Send message\" button or press \"Cancel\" to exit review writing mode", reply_markup=markup)
-
+    bot.send_message(message.chat.id, "Please, write your review with \"#\" at the beggining in the next message ans press \"Send message\" button or press \"Cancel\" to exit review writing mode",
+                     reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    #Name case
+    # Name case
     if ((message.text.find("your") != -1) and (message.text.find("name")) != -1):
         bot.send_message(message.chat.id, 'I\'m omniscient bot - the most clever bot on telegram!')
         return
-    #Cancellation case
-    if(message.text == 'Cancel' or message.text == 'Send message'):
+    # Cancellation case
+    if (message.text == 'Cancel' or message.text == 'Send message'):
         return
-    #Review case
-    if(message.text[0] == '#'):
+    # Review case
+    if (message.text[0] == '#'):
         bot.send_message(443610747, message.text)
         bot.send_message(443610747, message.chat.id)
         try:
@@ -59,19 +64,20 @@ def send_text(message):
             except:
                 bot.send_message(443610747, "No first name\n")
         return
-    #Determine bot behaviour depending on chat type
+    # Determine bot behaviour depending on chat type
     if (message.text[0] != '!') and ((message.chat.type == 'supergroup') or (message.chat.type == 'group')):
         return
     if (message.text[0] == '!') and ((message.chat.type == 'supergroup') or (message.chat.type == 'group')):
         q = message.text[1:]
     else:
         q = message.text
-    #Getting answer
+    # Getting answer
     client = wolframalpha.Client('7HXRW2-X5A2AXYYPY')
     res = client.query(q)
     try:
         bot.send_message(message.chat.id, (next(res.results).text))
     except:
         bot.send_message(message.chat.id, 'Sorry, I don\'t know.')
+
 
 bot.polling()
