6f2c1df350c294b42a6b734b9ece30371a524b11
Serhii Iljin <89367614+serhii-iljin@users.noreply.github.com>
2021-09-23 20:09:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 import wolframalpha
-bot = telebot.TeleBot('1930969642:AAEH0CC-EYezvPVOvuM45OEg4_L8Wjproxo')
+bot = telebot.TeleBot('1930969642:AAE_bGsOl_4OHlu-tk77nrqsEVm4r-a1kdg')
 
 @bot.message_handler(commands=['help'])
 def start_message(message):
